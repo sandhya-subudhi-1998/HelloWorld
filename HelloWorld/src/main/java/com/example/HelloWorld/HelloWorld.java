1b36eb8380f0421ea831287d79beb2ac55cc26d7
AS20171435 <AS20171435@APL-F8RB403>
2020-12-11 14:18:04 +0530

    commit

--- HelloWorld/src/main/java/com/example/HelloWorld/HelloWorld.java
+++ HelloWorld/src/main/java/com/example/HelloWorld/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorld.class, args);
 		System.out.println("Hello World");
+		System.out.print("Hiiii");
 	}
 
 }
